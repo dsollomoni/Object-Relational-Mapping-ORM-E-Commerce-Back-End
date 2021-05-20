05f420eec5a095e55299d0a9713e991a7b591f32
Danny Sollomoni <dsollomoni@gmail.com>
2021-05-19 22:53:31 -0400

    WIP

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,13 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category)
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.hasMany(ProductTag)
 // Tags belongToMany Products (through ProductTag)
+Tag.hasMany(ProductTag)
 
 module.exports = {
   Product,
--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,14 +3,68 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  const categories = await Product.findAll({
+    include: [
+      {
+        model: Category,
+        required: true
+      }
+    ],
+    // where: {
+    //   category_id: "id"
+    // }
+  });
+  let response = {}
+    categories.forEach((category) => {
+    // return {
+    //    category_name: category.dataValues.category.category_name, 
+    //    product_name: category.dataValues.product_name
+    //   }
+    if (response.hasOwnProperty(category.dataValues.category.category_name)){
+      response[category.dataValues.category.category_name].push(category.dataValues.product_name)
+    } else {
+      let t = []
+      t.push(category.dataValues.product_name)
+      response[category.dataValues.category.category_name] = t
+    }
+   })
+   console.log(response)
+   res.json(response).status(200);
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  const categories = await Product.findAll({
+    include: [
+      {
+        model: Category,
+        required: true
+      }
+    ],
+     where: {
+       category_id: req.params.id
+     }
+  });
+  let response = {}
+    categories.forEach((category) => {
+    // return {
+    //    category_name: category.dataValues.category.category_name, 
+    //    product_name: category.dataValues.product_name
+    //   }
+    if (response.hasOwnProperty(category.dataValues.category.category_name)){
+      response[category.dataValues.category.category_name].push(category.dataValues.product_name)
+    } else {
+      let t = []
+      t.push(category.dataValues.product_name)
+      response[category.dataValues.category.category_name] = t
+    }
+   })
+   console.log(response)
+   res.json(response).status(200);
 });
 
 router.post('/', (req, res) => {
